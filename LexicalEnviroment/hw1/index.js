337eceb04954d49b733b75b2446a9d9009ba14ef
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-21 19:02:49 +0300

    init files

---
+++ LexicalEnviroment/hw1/index.js
@@ -0,0 +1,30 @@
+memory = 0;
+
+    export function add(num) {
+        memory += num;
+        console.log(memory);   
+    }
+
+    export function decrease(num) {
+        memory -= num;
+        console.log(memory);  
+    }
+
+    export function reset() {
+        memory = 0;
+        console.log(memory);  
+    }
+
+    export function getMemo() {
+        return memory;
+    }
+
+// add(10);
+// 
+// decrease(5);
+// 
+// reset();
+// 
+// add(100);
+// 
+// getMemo();
