5b530eb14b7bced149a068e94e9b5f46cefad325
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-21 20:43:25 +0300

    init files

--- Closure/hw2/index.js
+++ Closure/hw2/index.js
@@ -1,47 +1,54 @@
-let memoty = [];
+// let memory = [];
 
-export function createLoger() {
-    
-    function warn(message) {
+
+export function createLogger() {
+    let memory = [];
+
+    function warn(elem) {
         memory.push({
-            message: message,
+            message: elem,
             dateTime: new Date(),
-            type: 'warn'
-        })
-    }
+            type: 'warn',
+        });
+    };
 
-    function error(message) {
+    function error(elem) {
         memory.push({
-            message: message,
+            message: elem,
             dateTime: new Date(),
-            type: 'error'
-        })
-    }
+            type: 'error',
+        });
+    };
 
-    function log(message) {
+    function log(elem) {
         memory.push({
-            message: message,
+            message: elem,
             dateTime: new Date(),
-            type: 'log'
-        })
-    }
+            type: 'log',
+        });
+    };
 
-    function getRecords( type ) {
+    function getRecords(type) {
+        // console.log(type)
+        // if (type !== undefined) {
+        //     let result = memory.filter(el => el.type === type);
+        //     return result;
+        // }
 
-        if( type !== undefined ) {
-            const result = memory.filter(el => el.type === type)
-                .sort((a, b) => b.dateTime - a.dateTime);
+        if (type !== undefined) {
+            let result = memory.filter(el => el.type === type)
+            .sort((a, b) => b.dateTime - a.dateTime);
+            console.log(result)
             return result;
         } else {
-            return memory.sort((a, b) => b.dateTime - a.dateTime)
-        }
-        
-    }
+            return memory.sort((a, b) => b.dateTime - a.dateTime);
+        };
+    };
 
     return {
         warn,
         error,
         log,
-        getRecords
-    }
-}
+        getRecords,
+    };
+};
