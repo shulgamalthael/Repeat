bdb8c9670acdfdc35d8eaed09f9320c85e767557
Shulha Nikita <shulgamalthael@gmail.com>
2020-10-21 20:40:13 +0300

    init files

--- Closure/hw2/index.js
+++ Closure/hw2/index.js
@@ -1,57 +1,41 @@
-const memory = [
-    {
-        message: '',
-        dateTime: new Date(),
-        type: 'warn'
-    },
-    {
-        message: '',
-        dateTime: new Date(),
-        type: 'error'
-    },
+let memoty = [];
 
-    {
-        message: '',
-        dateTime: new Date(),
-        type: 'log'
-    }
-];
-
-function validate(type, str) {
-    memory.map( elem => {
-        if( elem.type === type ) {
-            return elem.message = str;
-        }
-    })
-}
-
-export function createLogger() {
-    function warn(str) {
-        validate('warn', str);
-    }
-
-    function error(str) {
-        validate('error', str);
-    }
-
-    function log(str) {
-        validate('log', str);
-    }
-
-    function getRecords(type) {
-
-        if( type === undefined ) {
-            console.log(memory);
-            const sortedMemory = memory.sort((a, b) => {
-                return a.dateTime - b.dateTime;
-            })
-        }
-
-        memory.map(elem => {
-            if( elem.type === type ) {
-                console.log( elem );
-            }
+function createLoger() {
+    
+    function warn(message) {
+        memory.push({
+            message: message,
+            dateTime: new Date(),
+            type: 'warn'
         })
+    }
+
+    function error(message) {
+        memory.push({
+            message: message,
+            dateTime: new Date(),
+            type: 'error'
+        })
+    }
+
+    function log(message) {
+        memory.push({
+            message: message,
+            dateTime: new Date(),
+            type: 'log'
+        })
+    }
+
+    function getRecords( type ) {
+
+        if( type !== undefined ) {
+            const result = memory.filter(el => el.type === type)
+                .sort((a, b) => b.dateTime - a.dateTime);
+            return result;
+        } else {
+            return memory.sort((a, b) => b.dateTime - a.dateTime)
+        }
+        
     }
 
     return {
@@ -61,12 +45,3 @@ export function createLogger() {
         getRecords
     }
 }
-
-// console.log(memory);
-// 
-// const loger = createLogger();
-// loger.warn('Hi!');
-// loger.error('Error');
-// loger.log('Success');
-// loger.getRecords('log');
-// loger.getRecords();
